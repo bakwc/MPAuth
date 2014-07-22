a71ee67a024f0700aa6f00c7e43b6f138efa8140
Filipp <ozinov@quantion.ru>
2014-07-22 17:42:24 +0400

    fixed styleguide

--- mpauth.c
+++ mpauth.c
@@ -29,9 +29,9 @@ MODULE_LICENSE("GPL");
 
 struct nf_hook_ops bundle;
 
-static int pingPass=220;
+static int pingPass = 220;
 static int timeToConnect;
-static int filterPort=22;
+static int filterPort = 22;
 static __be32 allowedIp;
 static __kernel_time_t allowedTime;
 
@@ -39,51 +39,43 @@ module_param(pingPass, int, S_IRUSR);
 module_param(filterPort, int, S_IRUSR);
 
 unsigned int intercept(unsigned hooknum,
-                  struct sk_buff *skb,
-                  const struct net_device *in,
-                  const struct net_device *out,
-                  int (*okfn)(struct sk_buff *)  )
+                       struct sk_buff* skb,
+                       const struct net_device* in,
+                       const struct net_device* out,
+                       int (*okfn)(struct sk_buff*)  )
 {
-    struct iphdr *ip;
+    struct iphdr* ip;
     struct timeval currTime;
-    struct tcphdr *tcp;
+    struct tcphdr* tcp;
 
     do_gettimeofday(&currTime);
 
-    if (skb->protocol == htons(ETH_P_IP))
-    {
-	ip = (struct iphdr *) skb_network_header(skb);
+    if (skb->protocol == htons(ETH_P_IP)) {
+        ip = (struct iphdr *) skb_network_header(skb);
 
-        if (ip->protocol == IPPROTO_ICMP)
-	{
-            if (skb->len==pingPass)
-            {
+        if (ip->protocol == IPPROTO_ICMP) {
+            if (skb->len==pingPass) {
                 allowedIp=ip->saddr;
                 allowedTime=currTime.tv_sec;
                 //printk("Access granted..\n");
             }
-	}
+        }
 
-        if (ip->protocol == IPPROTO_TCP)
-        {
+        if (ip->protocol == IPPROTO_TCP) {
             skb_set_transport_header(skb, ip->ihl * 4);
-            tcp = (struct tcphdr *)skb_transport_header(skb);
+            tcp = (struct tcphdr*)skb_transport_header(skb);
 
-            if (tcp->dest == htons(filterPort))
+            if (tcp->dest == htons(filterPort) &&
+                tcp->syn)
             {
-                if (tcp->syn)
+                if (ip->saddr == allowedIp &&
+                    currTime.tv_sec < (allowedTime + timeToConnect))
                 {
-                    if (ip->saddr==allowedIp &&
-                        currTime.tv_sec<(allowedTime+timeToConnect))
-                    {
-                        //printk("Allowed\n");
-                        return NF_ACCEPT;
-                    }
-                    else
-                    {
-                        //printk("Disallowed!\n");
-                        return NF_DROP;
-                    }
+                    //printk("Allowed\n");
+                    return NF_ACCEPT;
+                } else {
+                    //printk("Disallowed!\n");
+                    return NF_DROP;
                 }
             }
         }
@@ -92,26 +84,25 @@ unsigned int intercept(unsigned hooknum,
     return NF_ACCEPT;
 }
 
-int Init(void)
-{
-    timeToConnect=30;
-    allowedIp=0;
+int Init(void) {
+    timeToConnect = 30;
+    allowedIp = 0;
 
-    pingPass+=28;
+    pingPass += 28;
 
     bundle.hook = intercept;
     bundle.owner = THIS_MODULE;
     bundle.pf = PF_INET;
     bundle.hooknum = NF_INET_PRE_ROUTING;
     bundle.priority = NF_IP_PRI_FIRST;
+    
     nf_register_hook(&bundle);
 
     printk("MPAuth loaded\n");
     return 0;
 }
 
-void Exit(void)
-{
+void Exit(void) {
     nf_unregister_hook(&bundle);
     printk("MPAuth unloaded\n");
 }
